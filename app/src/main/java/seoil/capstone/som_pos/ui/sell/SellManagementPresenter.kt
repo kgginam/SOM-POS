ac7e67466b4e0aa60bf8b85b282abef04bd2f231
kgginam <kgginam00@naver.com>
2021-07-28 19:09:55 +0900

    fix

--- app/src/main/java/seoil/capstone/som_pos/ui/sell/SellManagementPresenter.kt
+++ app/src/main/java/seoil/capstone/som_pos/ui/sell/SellManagementPresenter.kt
@@ -135,7 +135,7 @@ class SellManagementPresenter: SellManagementContract.Presenter{
                             }
 
                             mView!!.setStock(results)
-                        } else if (t.status ==StockApi.ERROR_NONE_DATA) {
+                        } else if (t.status == StockApi.ERROR_NONE_DATA) {
 
                             val results: ArrayList<DataModel.StockData> = ArrayList()
                             mView!!.setStock(results)
